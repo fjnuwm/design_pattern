ede34c2d7a15da01f342e6faec868a362b77a65f
mingwang <mingwang@yangqianguan.com>
2019-09-03 22:23:50 +0800

    抽象工厂模式

--- factory/src/main/java/com/learn/operator/factorymethod/OperatorFactoryClient.java
+++ factory/src/main/java/com/learn/operator/factorymethod/OperatorFactoryClient.java
@@ -3,6 +3,9 @@ package com.learn.operator.factorymethod;
 import com.learn.operator.factorymethod.impl.AddOperatorFactory;
 import com.learn.operator.operators.IOperator;
 
+/**
+ * 工厂方法模式
+ */
 public class OperatorFactoryClient {
   public static void main(String[] args) {
     IOperatorFactory addOperatorFactory = new AddOperatorFactory();
--- factory/src/main/java/com/learn/operator/simplefactory/OperatorFactoryClient.java
+++ factory/src/main/java/com/learn/operator/simplefactory/OperatorFactoryClient.java
@@ -2,6 +2,9 @@ package com.learn.operator.simplefactory;
 
 import com.learn.operator.operators.IOperator;
 
+/**
+ * 简单工厂模式
+ */
 public class OperatorFactoryClient {
   public static void main(String[] args) {
     String add = "+";
